901144c212d2b87252ece16ceaa2a220c201e0f6
agencik007 <agencik007@gmail.com>
2022-08-28 18:52:08 +0200

    Added fox service and dto for create fox

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FoxController } from './fox/fox.controller';
+import { FoxService } from './fox/fox.service';
 
 @Module({
   imports: [],
   controllers: [AppController, FoxController],
-  providers: [AppService],
+  providers: [AppService, FoxService],
 })
 export class AppModule {}
